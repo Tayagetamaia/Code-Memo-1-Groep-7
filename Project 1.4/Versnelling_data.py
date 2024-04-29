1e1ed00a8c0fdebf50728e495f61ad2ca191cfca
Guus Hulst <69253666+Buusta@users.noreply.github.com>
2024-04-29 14:27:42 +0200

    Finished differential equation

--- Project 1.4/Versnelling_data.py
+++ Project 1.4/Versnelling_data.py
@@ -6,13 +6,39 @@ Created on Fri Apr 26 09:33:21 2024
 """
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('Data\posities\posities_1_Team_07.txt', delim_whitespace=True)
+# paramaters
+k = 42  # N / m
+m = 1.9372e-3  # kg
+y = 0.045101  # kg / s
+
+# read in data
+data = pd.read_csv('Data\posities\posities_1_Team_07.txt', sep='\s+')
 
 
+
+# make velocity and acceleration columns and fill Nans with 0
 data["v (m/s)"] = (data["x"] - data["x"].shift(1)) / (data["t"] - data["t"].shift(1))
 data["a (m/s^2)"] = (data["v (m/s)"] - data["v (m/s)"].shift(1)) / (data["t"] - data["t"].shift(1))
+data = data.fillna(0)
+
+# make array with time data
+t = np.array(data["t"].values)
+
+# get acceleration and make empty arrays for Eulers method
+xf = np.array(data["a (m/s^2)"].values)
+x = np.zeros_like(t)
+x_prime = np.zeros_like(t)
 
 
-plt.plot(data["t"], data["a (m/s^2)"])
+# apply euelers method
+for i in range(len(t)-1):
+    x_2prime = (m*xf[i] - y*x_prime - k * x[i]) / m
+    x_prime[i+1] = x_prime[i] + (t[i+1] - t[i]) * x_2prime[i]
+    x[i+1] = x[i] + (t[i+1] - t[i]) * x_prime[i]
+                
+# plot the acceleration and differential equation
+plt.plot(data['t'], data['a (m/s^2)'] * 0.00005) # * 0.0005 scales the values down so it fits in one graph
+plt.plot(data["t"], x)
